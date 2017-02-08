cab2b37b4dbeaab3343818e2cf3c352748f08e63
jiangyayi <yayi.jiang@huangjinqianbao.com>
2017-02-08 15:46:12 +0800

    add newcode

--- src/main/java/apptest2.java
+++ src/main/java/apptest2.java
@@ -73,17 +73,9 @@ public class apptest2 {
             sleep(100);
             wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAButton[1]")).click();
             sleep(1000);
-
-//            SwipUtils.swipeToLeft((AppiumDriver<WebElement>) wd,500,1);
-//            sleep(100);
-//            System.out.println("滑屏成功4");
-
-//            //新手窗点xx
-//            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAButton[1]")).click();
-//            sleep(100);
 //
 
-
+            //上下滑屏
             SwipUtils.swipeToUp((AppiumDriver<WebElement>) wd,500,1);
             SwipUtils.swipeToDown((AppiumDriver<WebElement>) wd,500,2);
             sleep(100);
@@ -94,38 +86,98 @@ public class apptest2 {
             System.out.println(wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIAElement[2]")));
             wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIAElement[2]")).click();
 
-            //System.out.print(wd.getPageSource());
+
+            sleep(2000);
+            //新手金回退至首页
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[2]")).click();
+
             sleep(100);
+            SwipUtils.swipeToUp((AppiumDriver<WebElement>) wd,500,1);
+
+            sleep(100);
+            System.out.println("新手金结束");
 //
-//            //回退
-//            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[1]")).click();
-//            sleep(100);
+            //下边栏 点击全部 发现 和我的
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATabBar[1]/UIAButton[2]")).click();
+            sleep(200);
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATabBar[1]/UIAButton[3]")).click();
+            sleep(200);
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATabBar[1]/UIAButton[4]")).click();
+            System.out.println("下部按钮遍历成功");
+
+            //滑屏到上方
+            SwipUtils.swipeToDown((AppiumDriver<WebElement>) wd,500,2);
+            sleep(100);
+
+            //点击登录
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAImage[4]")).click();
+             //输入用户信息
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextField[1]")).sendKeys("15001141469");
+            sleep(100);
+            System.out.println("输入用户信息成功");
+
+            //点下一步
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAButton[2]")).click();
+            sleep(100);
+            System.out.println("下一步成功");
+
+            //输入密码
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIASecureTextField[1]")).sendKeys("123456");
+            sleep(100);
+            System.out.println("输入密码成功");
+
+            //点登录
+           wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAButton[2]")).click();
+            sleep(10000);
+
+            //下边栏 点击首页
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATabBar[1]/UIAButton[1]")).click();
+            sleep(200);
 //
-//            SwipUtils.swipeToLeft((AppiumDriver<WebElement>) wd,500,1);
+            //点买金
+            SwipUtils.swipeToDown((AppiumDriver<WebElement>) wd,500,1);
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIAButton[1]")).click();
+            sleep(100);
+
+            //输入买金克重
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextField[1]")).sendKeys("0.1");
+            sleep(100);
+
+            //点击确认下单
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAButton[1]")).click();
+            sleep(10);
+            //点击确认支付
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAButton[1]")).click();
+            sleep(10);
+
+            //输入交易密码
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIASecureTextField[1]")).sendKeys("000000");
+            sleep(100);
+            //点击确认
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAButton[1]")).click();
+            sleep(100);
+
+
+            //点击完成
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAButton[1]")).click();
+            sleep(100);
+
+
+            //点击回退
+            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[2]")).click();
+            sleep(100);
+
+//            //输入买金克重
+//            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextField[1]/")).sendKeys("0.1");
 //            sleep(100);
-//            System.out.println("滑屏成功6");
-//            //点买金
-//            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/UIAButton[1]")).click();
+//            //输入买金克重
+//            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextField[1]/")).sendKeys("0.1");
 //            sleep(100);
+            sleep(100000);
+            System.out.println("结束");
+
 //
-//            SwipUtils.swipeToLeft((AppiumDriver<WebElement>) wd,500,1);
-//            sleep(100);
-//            System.out.println("滑屏成功7");
-//            //输入用户信息
-//            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIATextField[1]/UIATextField[1]")).sendKeys("15001141469");
-//            sleep(100);
-//
-//            //点下一步
-//            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAButton[2]")).click();
-//            sleep(100);
-//
-//            //输入密码
-//            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIASecureTextField[1]/UIASecureTextField[1]")).sendKeys("123456");
-//            sleep(100);
-//
-//            //点登录
-//           wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[1]/UIAButton[2]")).click();
-//            sleep(100);
+
 ////
 
 //            wd.findElement(By.xpath("//UIAApplication[1]/UIAWindow[2]/UIATextField[1]").xpath("//UIAApplication[1]/UIAWindow[2]/UIATextField[1]/UIATextField[1]")).sendKeys("100000");
---
+++ src/main/java/utils.java
@@ -0,0 +1,8 @@
+/**
+ * Created by gloria on 17/2/8.
+ */
+public class utils {
+
+
+    //System.out.print(wd.getPageSource());打印全部页面元素
+}
